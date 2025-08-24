34f74247cf5ab30f25d78d3ab07a521367a498c0
TanWeiKhye <charlestanweikhye@gmail.com>
2025-08-24 14:23:27 +0800

    - Rename into "TreeInterface.java"

---
+++ src/java/adt/TreeInterface.java
@@ -0,0 +1,22 @@
+package adt;
+
+public interface TreeInterface<T> {
+	
+    // Inserts a new element into the tree, maintaining balance.
+    void insert(T data);
+
+    // Removes an element from the tree, rebalancing as needed.
+    void delete(T data);
+
+    // Searches for a specific element in the tree.
+    T search(T data);
+
+    // Finds and returns the minimum element in the tree.
+    T getMin();
+
+    // Finds and returns the maximum element in the tree.
+    T getMax();
+
+	// Checks if the tree is empty.
+    boolean isEmpty();
+}
