b39f1c44167f0df50bce0145d7ebf273d952a469
TanWeiKhye <charlestanweikhye@gmail.com>
2025-08-24 18:04:39 +0800

    Add Traverse Method

--- src/java/adt/TreeInterface.java
+++ src/java/adt/TreeInterface.java
@@ -10,7 +10,16 @@ public interface TreeInterface<T> {
 
     // Searches for a specific element in the tree.
     T search(T data);
-
+    
+    // Visit every node in chronologically or ascending order. (Left -> Root -> Right)
+    void traverseInOrder();
+    
+    // Visit every node in order. (Root -> Left -> Right) [eg. Save or copy a tree]
+    void traversePreOrder();
+    
+    // Visit every node in order. (Left -> Right -> Root) [eg. Delete/free a tree safely]
+    void traversePostOrder();
+    
     // Finds and returns the minimum element in the tree.
     T getMin();
 
